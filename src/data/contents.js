3d4efe99439ed677580819e86cdfb7d2b4c182d0
Reander Agulto <reanderagulto29@gmail.com>
2023-11-02 16:33:40 +0800

    Delete all SVG Files. Transfer it to one component

---
+++ src/data/contents.js
@@ -0,0 +1,83 @@
+export const siteInfo = {
+    siteName: "Startup 3", 
+    phoneNumber: "+1 555 505 5050", 
+    email: "info@designmodo.com", 
+    address: "San Francisco, CA560 Bush St & 20th Ave, Apt5 San Francisco, 230909", 
+    copyright: "© 2017 Designmodo. All rights reserved."
+};
+
+export const listOfReviews = [
+    {
+        name: "Rayhan Curran", 
+        image: "/images/mask-1.png", 
+        review: "Get a fully retina ready site when you build with Startup Framework. Websites look sharper and more gorgeous on devices with retina display support.",
+    }, 
+    {
+        name: "Kayley Frame", 
+        image: "/images/mask-2.png", 
+        review: "As a business targeting high net worth individuals, we were looking for a slick, cool and mini-malistic design for our website.",
+    }, 
+    {
+        name: "Gene Whitfield", 
+        image: "/images/mask-3.png", 
+        review: "The most important part of the Startup Framework is the samples.",
+    }, 
+    {
+        name: "Allan Kim", 
+        image: "/images/mask-4.png", 
+        review: "I’ve built my website with Startup just in one day, and it was ready-to-go.",
+    }, 
+];
+
+export const listOfWorks = [
+    {
+        name: "Mozart Project", 
+        category: "UI kit", 
+        image: "/images/mozart-project.png"
+    }, 
+    {
+        name: "Startup Framework 2.0", 
+        category: "Framework", 
+        image: "/images/setup-framework.png"
+    }, 
+    {
+        name: "From the Sky", 
+        category: "Photos", 
+        image: "/images/from-the-sky.png"
+    },
+    {
+        name: "Air Forces", 
+        category: "Pictures", 
+        image: "/images/air-forces.png"
+    },
+];
+
+export const crew = [
+    {
+        name: "Leah Salomon", 
+        position: "UI Designer", 
+        image: "/images/mask-1.png",
+        socials: [
+            {
+                name: "twitter", 
+                link: "https://twitter.com", 
+                icon: "@assets/icons/fa-twitter.svg"
+            },
+            {
+                name: "facebook", 
+                link: "https://facebook.com", 
+                icon: "@assets/icons/fa-facebook-square.svg"
+            }, 
+            {
+                name: "dribble", 
+                link: "https://dribble.com", 
+                icon: "@assets/icons/fa-dribble.svg"
+            },
+            {
+                name: "instagram", 
+                link: "https://instagram.com", 
+                icon: "@assets/icons/fa-instagram.svg"
+            }, 
+        ]
+    }
+];
---
+++ src/data/menus.js
@@ -0,0 +1,77 @@
+export const headerMenu = [
+    {
+        name: "Home",
+        uri: "/",
+        isIcon: false
+    }, 
+    {
+        name: "Features",
+        uri: "/features",
+        isIcon: false
+    }, 
+    {
+        name: "pricing", 
+        uri: "/pricing", 
+        isIcon: false
+    }, 
+    {
+        name: "blog", 
+        uri: "/blog",
+        isIcon: false
+    }, 
+    {
+        name: "dribble", 
+        uri: "/dribble",
+        isIcon: true
+    }, 
+    {
+        name: "behance", 
+        uri: "/behance", 
+        isIcon: true
+    }
+];
+
+export const footerMenu = [
+    {
+        name: "Tour", 
+        uri: "/tour", 
+    }, 
+    {
+        name: "Features",
+        uri: "/features"
+    }, 
+    {
+        name: "Pricing Plans", 
+        uri: "/pricing"
+    }, 
+    {
+        name: "Our Works", 
+        uri: "/our-works"
+    }, 
+    {
+        name: "Brands", 
+        uri: "/brands"
+    }, 
+    {
+        name: "Contacts", 
+        uri: "/contacts"
+    }
+];
+
+export const socialLinks = [
+    {
+        name: "twitter", 
+        uri: "https://www.twitter.com",
+        icon: "@assets/images/fa-twitter.svg"
+    }, 
+    {
+        name: "facebook", 
+        uri: "https://www.facebook.com",
+        icon: "@assets/images/fa-facebook.svg",
+    }, 
+    {
+        name: "google-plus", 
+        uri: "https://plus.google.com",
+        icon: "@assets/images/fa-google-plus.svg"
+    }
+];
